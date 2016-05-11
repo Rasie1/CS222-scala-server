3d1b4f51a1533449e8fada4a7220a3ba2f7f5b71
Vsevolod Kvachev <rasielll@gmail.com>
2016-05-12 00:28:43 +0300

    tries

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,28 @@
-/**
-  * Created by rasiel on 28.04.16.
-  */
+import java.io._
+import java.net.{ServerSocket, Socket}
+
 object Main {
+  class ProcessClient(socket : Socket) extends Runnable {
+    def run() = {
+      val out = new PrintWriter(socket.getOutputStream(), true)
+      val in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
 
-  def main(args: Array[String]): Unit = {
-
-    import java.net._
-    import java.io._
-    import scala.io._
-
-    val server = new ServerSocket(9999)
-    while (true) {
-      val s = server.accept()
-      val in = new BufferedSource(s.getInputStream()).getLines()
-      val out = new PrintStream(s.getOutputStream())
-
-      val answer = "HTTP/1.1 200 OK\nServer: super fast mexmat server\nContent-Type: text/html\nContent-Length: 1\n\n";
-      // out.println(answer)
-      out.println("Answer:\n" ++ in.next() ++ "\n/answer")
-      out.flush()
-      s.close()
+      var inputLine = in.readLine()
+      while (inputLine != null && inputLine != "\n\n")
+      {
+        val answer = "HTTP/1.1 200 OK\nServer: super fast mexmat server\nContent-Type: text/html\nContent-Length: 1\n\n"
+        out.println(answer + inputLine.charAt(7))
+        out.flush()
+        inputLine = in.readLine()
+      }
+      socket.close()
     }
   }
 
+  def main(args: Array[String]): Unit = {
+    val server = new ServerSocket(19001)
+    while (true) {
+      new Thread(new ProcessClient(server.accept())).start()
+    }
+  }
 }
